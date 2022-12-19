c8d437db216ed0d5bcac09992e3b3159488af8c6
sjeon-elground <sjeon@elground.com>
2022-12-19 16:22:11 +0900

    moved query cleaning into levenshtein distance

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { getLshBatchesFromQueryString } from '#src/models/get-lsh-batches-from-query-string'
 export { sortByNearestCandidate } from '#src/models/sort-by-nearest-candidate'
+export { getNormalizedLevenshteinDistance } from '#src/models/sort-by-nearest-candidate/get-normalized-levenshtein-distance'
--- src/models/sort-by-nearest-candidate/get-normalized-levenshtein-distance.ts
+++ src/models/sort-by-nearest-candidate/get-normalized-levenshtein-distance.ts
@@ -1,3 +1,5 @@
+import hangul from 'hangul-js'
+
 export const getNormalizedLevenshteinDistance = (
   queryStringA: string,
   queryStringB: string,
@@ -6,12 +8,36 @@ export const getNormalizedLevenshteinDistance = (
   if (!queryStringA) return queryStringB.length
   if (!queryStringB) return queryStringA.length
 
-  const matrix: number[][] = [...Array(queryStringB.length + 1)].map((x) =>
-    Array(queryStringA.length + 1).fill(0),
+  const cleanedQueryStringA: string = queryStringA.replace(
+    /[\x00-\x2F|\x3A-\x40|\x5B-\x5E|\x60|\x7B-\x7F]/gm,
+    '',
+  )
+  const cleanedQueryStringB: string = queryStringB.replace(
+    /[\x00-\x2F|\x3A-\x40|\x5B-\x5E|\x60|\x7B-\x7F]/gm,
+    '',
   )
 
-  for (let rowIndex = 0; rowIndex < queryStringB.length + 1; rowIndex++) {
-    for (let colIndex = 0; colIndex < queryStringA.length + 1; colIndex++) {
+  const disassembledQueryStringA: string = hangul
+    .disassemble(cleanedQueryStringA)
+    .join('')
+  const disassembledQueryStringB: string = hangul
+    .disassemble(cleanedQueryStringB)
+    .join('')
+
+  const matrix: number[][] = [
+    ...Array(disassembledQueryStringB.length + 1),
+  ].map((x) => Array(disassembledQueryStringA.length + 1).fill(0))
+
+  for (
+    let rowIndex = 0;
+    rowIndex < disassembledQueryStringB.length + 1;
+    rowIndex++
+  ) {
+    for (
+      let colIndex = 0;
+      colIndex < disassembledQueryStringA.length + 1;
+      colIndex++
+    ) {
       // initialize levenshtein matrix such that the top-most row is in ascending order (0,1,2,3,...)
       if (rowIndex === 0) {
         matrix[0][colIndex] = colIndex
@@ -27,7 +53,8 @@ export const getNormalizedLevenshteinDistance = (
       // increment cell values based on neighboring cells
       // calculates least amount of steps(addition, deletion, substitution) required to change from one substring to another
       matrix[rowIndex][colIndex] =
-        queryStringB.charAt(rowIndex - 1) == queryStringA.charAt(colIndex - 1)
+        disassembledQueryStringB.charAt(rowIndex - 1) ==
+        disassembledQueryStringA.charAt(colIndex - 1)
           ? matrix[rowIndex - 1][colIndex - 1]
           : Math.min(
               matrix[rowIndex - 1][colIndex - 1],
@@ -39,7 +66,7 @@ export const getNormalizedLevenshteinDistance = (
 
   // return normalized levenshtein distance
   return (
-    matrix[queryStringB.length][queryStringA.length] /
-    Math.max(queryStringA.length, queryStringB.length)
+    matrix[disassembledQueryStringB.length][disassembledQueryStringA.length] /
+    Math.max(disassembledQueryStringA.length, disassembledQueryStringB.length)
   )
 }
